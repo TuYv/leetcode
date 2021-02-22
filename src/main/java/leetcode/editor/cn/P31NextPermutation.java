b0ffbb413a0d4ca4a1014d6ef9664d039c32777b
涂瑜 <861506831@qq.com>
2021-02-23 01:44:08 +0800

    P31 下一个排列

--- src/main/java/leetcode/editor/cn/P31NextPermutation.java
+++ src/main/java/leetcode/editor/cn/P31NextPermutation.java
@@ -51,27 +51,38 @@ package leetcode.editor.cn;
 //下一个排列
 
 public class P31NextPermutation {
-	public static void main(String[] args) {
-		//测试代码
-		Solution solution = new P31NextPermutation().new Solution();
-		solution.nextPermutation(new int[]{4, 2, 0, 2, 3, 2, 0});
-	}
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P31NextPermutation().new Solution();
+        solution.nextPermutation(new int[] {1,3,2});
+    }
 
-	//力扣代码
-	//leetcode submit region begin(Prohibit modification and deletion)
-	class Solution {
-		public void nextPermutation(int[] nums) {
-			//1想法 从最后向前循环找比这个数字小的 如果全没有就逆过来
-			//2失败的想法
-			//3不 我没有错 少了一步 替换之后还需要对替换后的数组排个序
-			//4上面想法有问题 还是从最后 但是应该往后循环找出比他大的数组里最小的数 替换掉 之后再对后面的数排序
-			//5可以进一步把排序的逻辑在换算的时候做出来 去除多余代码
-			int length = nums.length;
-			if (length <= 1) {
-				return;
-			}
-			boolean flag = true;
-			//3
+    //力扣代码
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void nextPermutation(int[] nums) {
+            /*int n = nums.length;
+            int k = n - 1;
+            while (k - 1 >= 0 && nums[k - 1] >= nums[k]) k--;
+            if (k == 0) {
+                reverse(nums, 0, n - 1);
+            } else {
+                int u = k;
+                while (u + 1 < n && nums[u + 1] > nums[k - 1]) u++;
+                swap(nums, k - 1, u);
+                reverse(nums, k, n - 1);
+            }*/
+
+            //1想法 从最后向前循环找比这个数字小的 如果全没有就逆过来
+            //2失败的想法
+            //3不 我没有错 少了一步 替换之后还需要对替换后的数组排个序
+            //4上面想法有问题 还是从最后 但是应该往后循环找出比他大的数组里最小的数 替换掉 之后再对后面的数排序
+            //5可以进一步把排序的逻辑在换算的时候做出来 去除多余代码
+            int length = nums.length;
+            if (length <= 1) {
+                 return;
+            }
+            //3
 			/*int i = length - 1;
 			//只用触发一次 就结束了
 			while (flag && i >= 0) {
@@ -96,42 +107,74 @@ public class P31NextPermutation {
 				}
 				i--;
             }*/
-			int min = Integer.MAX_VALUE;
-			int min_index = length;
-			for (int i = length - 2; i >= 0; i--) {
-				int j = i + 1;
-				while (j < length) {
-					if (nums[i] < nums[j] && nums[j] < min) {
-						min = nums[j];
-						min_index = j;
-					}
-					j++;
-				}
-				if (min_index != length) {
-					nums[min_index] = nums[i];
-					nums[i] = min;
-					for (int x = i + 1; x < length; x++) {
-						for (int y = x + 1; y < length; y++) {
-							if (nums[y] < nums[x]) {
-								int temp2 = nums[y];
-								nums[y] = nums[x];
-								nums[x] = temp2;
-							}
-						}
-					}
-					break;
-				}
-			}
+			//用来记录i后面最小的的大于i的数字和它的下标
+            int i = length - 2;
+            //跳过从后面开始的升序元素 找到从后面开始数 第一个比它后面小的数字
+            while(i >= 0 && nums[i] >= nums[i + 1]) {
+                i--;
+            }
+            //降序数组 直接反转
+            if(i < 0) {
+                reverse(nums, 0, length - 1);
+                return;
+            }
+            //此时 i是达标的数字 i - 1 大于i 且 i后面是个降序数组
+            int min_index = i + 2;
+            //此时不能直接替换i和i - 1  还需要判断i - 1 后面有不有大于i的数字 有的话 用它来替换i
+            while(min_index < length && nums[i] < nums[min_index]) {
+                min_index++;
+            }
+            swap(nums, i, min_index - 1);
+            reverse(nums, i + 1, length - 1);
+            /*for (int i = length - 2; i >= 0; i--) {
+                int j = i + 1;
+                while (j < length) {
+                    if (nums[i] < nums[j] && nums[j] < min) {
+                        min = nums[j];
+                        min_index = j;
+                    }
+                    j++;
+                }
+                if (min_index != length) {
+                    nums[min_index] = nums[i];
+                    nums[i] = min;
+                    for (int x = i + 1; x < length; x++) {
+                        for (int y = x + 1; y < length; y++) {
+                            if (nums[y] < nums[x]) {
+                                exchange(nums, x, y);
+                            }
+                        }
+                    }
+                    return;
+                }
+            }
 
-			if (min_index == length) {
-				for (int x = 0, y = nums.length - 1; x < y; x++, y--) {
-					int temp = nums[x];
-					nums[x] = nums[y];
-					nums[y] = temp;
-				}
-			}
-		}
+            if (min_index == length) {
+                for (int x = 0, y = nums.length - 1; x < y; x++, y--) {
+                    exchange(nums, x, y);
+                }
+            }
+            return;*/
+        }
+
+
+        void reverse(int[] nums, int a, int b) {
+            int l = a, r = b;
+            while (l < r) {
+                swap(nums, l++, r--);
+            }
+        }
+        void swap(int[] nums, int a, int b) {
+            int c = nums[a];
+            nums[a] = nums[b];
+            nums[b] = c;
+        }
+        private void exchange(int[] nums, int x, int y) {
+            int temp = nums[x];
+            nums[x] = nums[y];
+            nums[y] = temp;
+        }
+
+    }
 //leetcode submit region end(Prohibit modification and deletion)
-
-	}
 }
