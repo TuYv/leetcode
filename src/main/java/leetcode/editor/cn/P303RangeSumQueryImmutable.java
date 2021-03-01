fcb2b9ea16959d77554326943516ccaca791c904
max <861506831@qq.com>
2021-03-01 14:45:39 +0800

    P303 区域和检索 - 数组不可变

---
+++ src/main/java/leetcode/editor/cn/P303RangeSumQueryImmutable.java
@@ -0,0 +1,83 @@
+//给定一个整数数组 nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。 
+//
+// 
+// 
+// 实现 NumArray 类： 
+//
+// 
+// NumArray(int[] nums) 使用数组 nums 初始化对象 
+// int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 s
+//um(nums[i], nums[i + 1], ... , nums[j])） 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//["NumArray", "sumRange", "sumRange", "sumRange"]
+//[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+//输出：
+//[null, 1, -1, -3]
+//
+//解释：
+//NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+//numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+//numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+//numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 104 
+// -105 <= nums[i] <= 105 
+// 0 <= i <= j < nums.length 
+// 最多调用 104 次 sumRange 方法 
+// 
+// 
+// 
+// Related Topics 动态规划 
+// 👍 265 👎 0
+
+
+package leetcode.editor.cn;
+
+//区域和检索 - 数组不可变
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P303RangeSumQueryImmutable{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 }
+//力扣代码
+	//leetcode submit region begin(Prohibit modification and deletion)
+class NumArray {
+	     //前缀和
+	     int sums[];
+    public NumArray(int[] nums) {
+        sums = new int[nums.length + 1];
+        sums[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
